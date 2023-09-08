0d7f5d596c06180956502178b2e68f75040d2a92
Rajesh Kannan <krishfredrick11@gmail.com>
2023-09-08 19:53:06 +0530

    minor fix

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { register , login, logout}  = require('../controllers/authController')
 
-router.post('/register', register);
+router.post('/register', register);/
 router.get('/logout', logout);
 router.post('/login', login);
 
